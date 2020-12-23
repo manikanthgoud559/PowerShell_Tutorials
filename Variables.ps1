d4b93ad6132e3712e9d4501d7baccd1d702dc0c9
manikanth goud <manikanthgoud123@gmail.com>
2020-12-23 18:35:00 +0530

    first commit

---
+++ Variables.ps1
@@ -0,0 +1,22 @@
+﻿$variable = $null
+$local = $null
+
+function local_variable{
+$global:local = "its variable in side the function"
+
+Write-Host $variable
+}
+
+$variable = "it is in global"
+local_variable
+
+$local
+
+Set-Variable -Name "password" -Value "Jarvis_iron_man"
+$password
+
+${give any value @ & *-+} = "sysmblos and variables"
+${give any value @ & *-+}
+
+Set-Variable 'const_var' -option Constant -Value 3000
+$const_var = 998
